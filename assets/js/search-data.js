5998500c2deb8a5a476c24379ac473daab0b637f
valeriepolad <38049887+valeriepolad@users.noreply.github.com>
2024-12-27 23:31:23 +0000

    Deploying to gh-pages from @ valeriepolad/valeriepolad.github.io@eeab04e0088b16b933fa8f8a8eba5a2746963e64 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,13 +51,6 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "";
               },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
             },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
